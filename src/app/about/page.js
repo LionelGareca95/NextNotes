8ff772fcf6b4c6a9a7bf492019d208730a6281c6
LionelGareca95 <lionelgareca@gmail.com>
2023-04-06 17:06:46 -0300

    creando Contexto

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,8 +1,15 @@
-import React from 'react'
+"use client";
+import { useTasks } from "@/context/TasksContext";
 
 const page = () => {
+
+    const values = useTasks()
+    console.log(values)
+
   return (
-    <div>page</div>
+    <div>
+        about page
+    </div>
   )
 }
 
---
+++ src/context/TasksContext.js
@@ -0,0 +1,15 @@
+'use client'
+import { createContext, useContext } from 'react'
+
+export const TaskContext = createContext()
+
+export const useTasks = () => {
+	const context = useContext(TaskContext)
+	if (!context) throw new Error('use Task must used within a provider')
+	return context
+}
+
+export const TaskProvider = ({ children }) => {
+	const tasks = []
+	return <TaskContext.Provider value={{ tasks }}>{children}</TaskContext.Provider>
+}
